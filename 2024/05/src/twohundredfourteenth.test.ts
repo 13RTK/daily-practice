535647ea134a73fe91764a26f4106ae8cac0043b
alex <907785012@qq.com>
2024-05-27 21:19:56 +0800

    Mon May 27 21:19:55 CST 2024 update by Alex

---
+++ 2024/05/src/twohundredfourteenth.test.ts
@@ -0,0 +1,37 @@
+function combinationSum2(candidates: number[], target: number): number[][] {
+  candidates.sort((o1, o2) => o1 - o2);
+  const resArr: number[][] = [];
+  const curCombine: number[] = [];
+
+  function getCombine(
+    candidates: number[],
+    curSum: number,
+    sum: number,
+    startIdx: number
+  ): void {
+    if (curSum > sum) {
+      return;
+    }
+
+    if (curSum === sum) {
+      resArr.push([...curCombine]);
+      return;
+    }
+
+    for (let i = startIdx; i < candidates.length; i++) {
+      if (i > startIdx && candidates[i - 1] === candidates[i]) {
+        continue;
+      }
+
+      curCombine.push(candidates[i]);
+
+      getCombine(candidates, curSum + candidates[i], sum, i + 1);
+
+      curCombine.pop();
+    }
+  }
+
+  getCombine(candidates, 0, target, 0);
+
+  return resArr;
+}
