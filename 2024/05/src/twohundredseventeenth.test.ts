64803c83fc31b0a017adca846cbc7206300caeae
alex <907785012@qq.com>
2024-05-30 21:11:04 +0800

    Thu May 30 21:11:03 CST 2024 update by Alex

---
+++ 2024/05/src/twohundredseventeenth.test.ts
@@ -0,0 +1,26 @@
+function permute(nums: number[]): number[][] {
+  const resArr: number[][] = [];
+  const curPermutation: number[] = [];
+
+  function getPermutation(nums: number[], mark: boolean[]): void {
+    if (nums.length === curPermutation.length) {
+      resArr.push([...curPermutation]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (!mark[i]) {
+        curPermutation.push(nums[i]);
+        mark[i] = true;
+
+        getPermutation(nums, mark);
+
+        curPermutation.pop();
+        mark[i] = false;
+      }
+    }
+  }
+
+  getPermutation(nums, new Array(nums.length).fill(false));
+  return resArr;
+}
